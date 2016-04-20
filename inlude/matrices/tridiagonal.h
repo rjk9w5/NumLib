3817a6eb825e95602f1a9f82d1cac88c04dd7401
Ryan Krattiger <rjk9w5@mst.edu>
2016-04-20 17:34:46 -0500

    Added tridiagonal matrix, multiplications are not optimized and default by returning dense

---
+++ inlude/matrices/tridiagonal.h
@@ -0,0 +1,99 @@
+/*
+ *  tridiagonal.h
+ *  Author: Ryan Krattiger
+ *
+ *  Namespace: numlib
+ *  Brief: TriDiag Matrix Form class.
+ */
+
+#ifndef TRIDIAGONAL_H_
+#define TRIDIAGONAL_H_
+
+#include <utility>
+#include "matrix.h"
+
+namespace numlib
+{
+  template <class Type>
+  class TriDiag;
+
+  template <class Type>
+  void
+  swap(
+    TriDiag<Type> &d1,
+    TriDiag<Type> &d2);
+
+  template <class Type>
+  TriDiag<Type>
+  eye(
+    std::size_t n);
+
+  template <class Type>
+  class TriDiag: public Matrix<Type,TriDiag>
+  {
+    public:
+      TriDiag(): data_(), n_(0) {};
+      TriDiag(
+        std::size_t const &n);
+      TriDiag(
+        std::size_t const &n,
+        std::size_t const &m);
+
+      TriDiag<Type>&
+      operator = (
+        TriDiag<Type> cpy);
+
+      TriDiag(
+        TriDiag<Type> const &src);
+      TriDiag(
+        Matrix<Type,TriDiag> const &src);
+      TriDiag(
+        TriDiag<Type> &&other);
+      ~TriDiag() {}
+
+      // Basic Matrix Math operations
+      template <template <class> class F>
+      Matrix<Type,F>& operator + (Matrix<Type,F> const &rhs) const;
+
+      template <template <class> class F>
+      TriDiag<Type>& operator += (Matrix<Type,F> const &rhs);
+
+      template <template <class> class F>
+      Matrix<Type,F>& operator - (Matrix<Type,F> const &rhs) const;
+
+      template <template <class> class F>
+      TriDiag<Type>& operator -= (Matrix<Type,F> const &rhs);
+
+      template <template <class> class F>
+      Matrix<Type,F>& operator * (Matrix<Type,F> const &rhs) const;
+
+      Vector<Type> operator * (Vector<Type> const &rhs) const;
+
+      // Basic Matrix projection, syntax like mat[{i,j}]
+      Type operator[](std::initializer_list<std::size_t> ij) const;
+      Type& operator[](std::initializer_list<std::size_t> ij);
+
+      // Clone idiom for copying
+      std::shared_ptr<TriDiag<Type>> clone() const;
+
+      // Dimension information access
+      std::size_t N() const;
+      std::size_t M() const;
+      bool checki(std::size_t const i) const;
+      bool checkj(std::size_t const j) const;
+
+      friend void swap <> (TriDiag<Type> &d1, TriDiag<Type> &d2);
+
+      // IO Operations
+      void print(std::ostream &out) const;
+
+    private:
+      numlib::Vector<Type> data_;
+      std::size_t n_;
+      constexpr static const Type __empty_const__ = 0;
+  }; // class TriDiag
+} // namespace numlib
+
+#include "tridiagonal.hpp"
+
+#endif
---
+++ inlude/matrices/tridiagonal.hpp
@@ -0,0 +1,287 @@
+/*
+ *  tridiagonal.hpp
+ *  Author: Ryan Krattiger
+ *
+ *  Namespace: numlib
+ *  Brief: TriDiag Matrix Form class implementation.
+ */
+
+template <class Type>
+numlib::TriDiag<Type>::TriDiag(
+  std::size_t const &n):
+  data_(3*n-2),
+  n_(n)
+{}
+
+template <class Type>
+numlib::TriDiag<Type>::TriDiag(
+  std::size_t const &n,
+  std::size_t const &m):
+  data_(3*n-2),
+  n_(n)
+{
+}
+
+template <class Type>
+numlib::TriDiag<Type>&
+numlib::TriDiag<Type>::operator = (
+  numlib::TriDiag<Type> cpy)
+{
+  swap(*this,cpy);
+
+  return *this;
+}
+
+template <class Type>
+numlib::TriDiag<Type>::TriDiag(
+  Matrix<Type,TriDiag> const &src):
+  TriDiag(static_cast<const TriDiag<Type>&>(src))
+{
+}
+
+template <class Type>
+numlib::TriDiag<Type>::TriDiag(
+  TriDiag<Type> const &src):
+  data_(src.data_),
+  n_(src.n_)
+{
+}
+
+template <class Type>
+numlib::TriDiag<Type>::TriDiag(
+  TriDiag<Type> &&other):
+  data_(std::move(other.data_)),
+  n_(other.n_)
+{}
+
+template <class Type>
+template <template <class> class F>
+numlib::Matrix<Type,F>&
+numlib::TriDiag<Type>::operator + (
+  Matrix<Type,F> const &rhs) const
+{
+  if(N()!=rhs.N()||M()!=rhs.M())
+    throw DimensionMismatch("Matrix addition with operator + failed");
+
+  Matrix<Type,F> *ret = new F<Type>(rhs);
+
+  for(std::size_t i=0;i<n_;++i)
+  {
+    for(std::size_t j=(i?i-1:i); j<(i+1<n_?i+1:i); ++j)
+    {
+      ret->operator[]({i,j}) += this->operator[]({i,j});
+    }
+  }
+  return *ret;
+}
+
+template <class Type>
+template <template <class> class F>
+numlib::TriDiag<Type>&
+numlib::TriDiag<Type>::operator += (
+  Matrix<Type,F> const &rhs)
+{
+  if(N()!=rhs.N()||M()!=rhs.M())
+    throw DimensionMismatch("Matrix addition with operator += failed");
+
+  for(std::size_t i=0;i<n_;++i)
+  {
+    for(std::size_t j=(i?i-1:i); j<(i+1<n_?i+1:i); ++j)
+    {
+      this->operator[]({i,j}) +=ret->operator[]({i,j});
+    }
+  }
+  return *this;
+}
+
+template <class Type>
+template <template <class> class F>
+numlib::Matrix<Type,F>&
+numlib::TriDiag<Type>::operator - (
+  Matrix<Type,F> const &rhs) const
+{
+  if(N()!=rhs.N()||M()!=rhs.M())
+    throw DimensionMismatch("Matrix addition with operator + failed");
+
+  Matrix<Type,F> *ret = new F<Type>(rhs);
+
+  for(std::size_t i=0;i<n_;++i)
+  {
+    for(std::size_t j=(i?i-1:i); j<(i+1<n_?i+1:i); ++j)
+    {
+      ret->operator[]({i,j}) -= this->operator[]({i,j});
+    }
+  }
+  return *ret;
+}
+
+template <class Type>
+template <template <class> class F>
+numlib::TriDiag<Type>&
+numlib::TriDiag<Type>::operator -= (
+  Matrix<Type,F> const &rhs)
+{
+  if(N()!=rhs.N()||M()!=rhs.M())
+    throw DimensionMismatch("Matrix addition with operator += failed");
+
+  for(std::size_t i=0;i<n_;++i)
+  {
+    for(std::size_t j=(i?i-1:i); j<(i+1<n_?i+1:i); ++j)
+    {
+      this->operator[]({i,j}) -=ret->operator[]({i,j});
+    }
+  }
+  return *this;
+}
+// Re-write to be more efficient for TriDiagonal Form matrices!
+template <class Type>
+template <template <class> class F>
+numlib::Matrix<Type,Dense>&
+numlib::TriDiag<Type>::operator * (
+  Matrix<Type,F> const &rhs) const
+{
+  if(M() != rhs.N())
+    throw numlib::DimensionMismatch(
+      "numlib::Dense<Type>operator *(Matrix<Type,F> const &rhs)");
+
+  Matrix<Type,Dense>* ret = new Dense<Type>(N(), rhs.M());
+
+  for(std::size_t i=0; i<N(); ++i)
+  {
+    for(std::size_t j=0; j<M(); ++j)
+    {
+      ret->operator[]({i,j}) = 0;
+      for(std::size_t k=0; k<N(); ++k)
+      {
+        ret->operator[]({i,j}) += this->operator[]({i,k})*rhs[{k,j}];
+      }
+    }
+  }
+
+  return *ret;
+}
+// Re-write to be more efficient for TriDiagonal Form matrices!
+template <class Type>
+numlib::Vector<Type>
+numlib::TriDiag<Type>::operator * (
+  Vector<Type> const &rhs) const
+{
+  if(M() != rhs.get_size())
+    throw DimensionMismatch("Dense::operator *(Vector): Vector");
+
+  Vector<Type> ret(rhs);
+
+  for(std::size_t i=0; i<N(); ++i)
+  {
+    ret[i] = 0;
+    for(std::size_t k=0; k<M(); ++k)
+    {
+      ret[i] += this->operator[]({i,k})*rhs[k];
+    }
+  }
+
+  return ret;
+}
+
+template <class Type>
+Type numlib::TriDiag<Type>::operator[](
+  std::initializer_list<std::size_t> ij) const
+{
+  if(ij.size() != 2 || *(ij.begin()) >= N() || *(ij.begin()+1) >= M())
+    throw RangeException("TriDiag op []");
+
+  std::size_t i(*(ij.begin())), j(*(ij.begin()+1));
+
+  if(abs(j-i)<=1)
+    return data_[3*i + static_cast<int>(j-i)];
+  else
+    return __empty_const__;
+}
+
+template <class Type>
+Type& numlib::TriDiag<Type>::operator[](
+  std::initializer_list<std::size_t> ij)
+{
+  if(ij.size() != 2 || *(ij.begin()) >= N() || *(ij.begin()+1) >= M())
+    throw RangeException("TriDiag op []");
+
+  std::size_t i(*(ij.begin())), j(*(ij.begin()+1));
+
+  if(abs(j-i)<=1)
+  {
+    return data_[3*i + static_cast<int>(j-i)];
+  }
+  else
+  {
+    Type* emp;
+    *emp = __empty_const__;
+    return *emp;
+  }
+}
+
+// Clone idiom for copying
+template <class Type>
+std::shared_ptr<numlib::TriDiag<Type>>
+numlib::TriDiag<Type>::clone() const
+{
+  return std::shared_ptr<TriDiag<Type>>(new TriDiag<Type>(*this));
+}
+
+template <class Type>
+std::size_t
+numlib::TriDiag<Type>::N() const
+{
+  return n_;
+}
+
+template <class Type>
+std::size_t
+numlib::TriDiag<Type>::M() const
+{
+  return n_;
+}
+
+template <class Type>
+inline bool
+numlib::TriDiag<Type>::checki(
+    std::size_t const i) const
+{
+  return (n_)&&(i<n_);
+}
+
+template <class Type>
+inline bool
+numlib::TriDiag<Type>::checkj(
+    std::size_t const j) const
+{
+  return (n_)&&(j<n_);
+}
+
+template <class Type>
+void numlib::swap(
+  TriDiag<Type> &d1,
+  TriDiag<Type> &d2)
+{
+  using std::swap;
+  std::swap(d1.data_,d2.data_);
+  std::swap(d1.n_, d2.n_);
+  return;
+}
+// Re-write to be more efficient for TriDiagonal Form matrices!
+template <class Type>
+void
+numlib::TriDiag<Type>::print(
+  std::ostream &out) const
+{
+  if(N()>0)
+  {
+    for(std::size_t i=0; i<N(); ++i)
+    {
+      out << std::setw(8*i) << "" << ' ';
+      out << std::left << std::setw(8) << std::scientific
+          << data_[i] << ' ';
+      out << '\n';
+    }
+  }
+  return;
+}
