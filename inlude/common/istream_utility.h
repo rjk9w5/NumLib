0b7d68bd5c106964f260eb10016de34e74d9d51c
Ryan Krattiger <rjk9w5@mst.edu>
2016-04-21 00:57:46 -0500

    istream checking utility

---
+++ inlude/common/istream_utility.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** *\
+ * istream_utility.h
+ *
+ *  Created on: Mar 12, 2016
+ *      Author: Ryan Krattiger (rjk9w5)
+ *       Brief: 
+\* ************************************************************************** */
+#ifndef ISTREAM_UTILITY_H_
+#define ISTREAM_UTILITY_H_
+
+#include <istream>
+#include "exceptions.h"
+
+/*
+ *    function: checkIstream()
+ *       brief: Checks if std::istream is still reading correctly
+ *       input: istream to be checked
+ *         pre: None
+ *        post: if the istream is bad, it will throw an oonm::Exception type
+ *      return: void
+ */
+
+namespace oonm
+{
+  void checkIstream(std::istream& in);
+}
+
+#endif /* ISTREAM_UTILITY_H_ */
