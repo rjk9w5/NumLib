162d453b13df5f08621c97b4c6ef3409154ab213
Ryan Krattiger <rjk9w5@mst.edu>
2016-04-30 18:28:00 -0500

    Tolerance control with adaptive precision based on input type

--- include/common/convergence.h
+++ include/common/convergence.h
@@ -2,26 +2,26 @@
 #define COMMON_CONVERGENCE_H_
 
 #include <cmath>
-#include "../typetraits.h"
+#include "typetraits/float_traits.h"
+#include "typetraits/promote_float.h"
 
 namespace numlib
 {
   namespace converge
   {
     template <class T>
-    T __tolerance__ = machine_precision<T>::eps;
-
-    template <class T>
-    struct tolerance 
+    struct tolerance
     {
-      typedef typename float_traits<T>::float_type ftype; 
-      ftype HYPER_FINE=1*__tolerance__<ftype>;
-      ftype SUPER_FINE=2*__tolerance__<ftype>;
-      ftype FINE=5*__tolerance__<ftype>;
-      ftype NORMAL=10*__tolerance__<ftype>;
-      ftype CORSE=100*__tolerance__<ftype>;
-      ftype SUPER_CORSE=1000*__tolerance__<ftype>;
-      ftype HYPER_CORSE=10000*__tolerance__<ftype>;
+      typedef typename float_traits<T>::type ftype;
+      constexpr static const ftype __tolerance__ = float_traits<ftype>::eps;
+
+      ftype HYPER_FINE=1*__tolerance__;
+      ftype SUPER_FINE=2*__tolerance__;
+      ftype FINE=5*__tolerance__;
+      ftype NORMAL=10*__tolerance__;
+      ftype CORSE=100*__tolerance__;
+      ftype SUPER_CORSE=1000*__tolerance__;
+      ftype HYPER_CORSE=10000*__tolerance__;
     };
   }
 }
