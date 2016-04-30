b9061ebe49d9a06149b685950f91df38be1c400d
Ryan Krattiger <ryan.krattiger1234@gmail.com>
2016-04-29 20:00:16 -0500

    Added typetrait handling for vector of floats

--- include/typetraits/arithmatic_traits.h
+++ include/typetraits/arithmatic_traits.h
@@ -2,6 +2,9 @@
 #ifndef TYPETRAITS_ARITHMATICTRAITS_H_
 #define TYPETRAITS_ARITHMATICTRAITS_H_
 
+#include "../vector/vector.h"
+
+// General float_trait
 template <class T>
 struct float_trait
 {
@@ -18,6 +21,26 @@ template <>
 struct float_trait<char>
 {
   typedef double T_float;
+};
+
+
+// Vector handling float_trait structs
+template <class T>
+struct float_trait<numlib::Vector<T>>
+{
+  typedef numlib::Vector<T> T_float;
+};
+
+template <>
+struct float_trait<numlib::Vector<int>>
+{
+  typedef numlib::Vector<double> T_float;
+};
+
+template <>
+struct float_trait<numlib::Vector<char>>
+{
+  typedef numlib::Vector<double> T_float;
 };
 
 #endif /* TYPETRAITS_ARITHMATICTRAITS_H_ */
