dce669fccc19d14b3b2c23b76aa32ed44b1108d3
Ryan Krattiger <rjk9w5@mst.edu>
2016-04-30 18:24:51 -0500

    Added start of derivative class

---
+++ include/derivatives/derivatives.h
@@ -0,0 +1,14 @@
+#ifndef DERIVATIVES_DERIVATIVES_H_
+#define DERIVATIVES_DERIVATIVES_H_
+
+#include "../common/convergence.h"
+#include "../derivatives/derivatives.h"
+#include <iostream>
+
+// template <class T, class TFunc>
+// typename promote_float<T>::type
+// modsecant(TFunc f, T x0, T x1, T x2);
+
+#include "derivatives.hpp"
+
+#endif
