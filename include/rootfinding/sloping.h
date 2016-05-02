aefc8b70f9396227c2fa5c3c485c9c2bc652db49
Ryan Krattiger <ryan.krattiger1234@gmail.com>
2016-05-01 20:17:07 -0500

    updates to secant method

--- include/rootfinding/sloping.h
+++ include/rootfinding/sloping.h
@@ -2,16 +2,16 @@
 #define ROOTFINDING_SLOPING_H_
 
 #include "../common/convergence.h"
-#include "../derivatives/derivatives.h"
+// #include "../derivatives/derivatives.h"
 #include <iostream>
 
 template <class T, class TFunc>
 typename promote_float<T>::type
 secant(TFunc f, T x0, T x1);
 
-template <class T, class TFunc>
-typename promote_float<T>::type
-modsecant(TFunc f, T x0, T x1, T x2);
+// template <class T, class TFunc>
+// typename promote_float<T>::type
+// modsecant(TFunc f, T x0, T x1, T x2);
 
 #include "sloping.hpp"
 
--- include/rootfinding/sloping.hpp
+++ include/rootfinding/sloping.hpp
@@ -7,13 +7,19 @@ secant(TFunc f, T x0, T x1)
   typename promote_float<T>::type err = 1;
   int max_it = 100, it=0;
 
-  while(err > tol.NORMAL && it++ < max_it)
+  while(err > tol.HYPER_FINE && it++ < max_it)
   {
     x_new = x1 - (f(x1)*(x1-x0))/(f(x1)-f(x0));
     x0 = x1;
     x1 = x_new;
     err = fabs(x1-x0)/fabs(x1);
   }
+
+  if(it >= max_it)
+  {
+    std::cerr << "Warning: Secant exited at max iterations of " << max_it << '\n';
+  }
+
   return x_new;
 }
 
